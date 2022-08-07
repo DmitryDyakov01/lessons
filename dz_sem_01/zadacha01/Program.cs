c7659705cb2711e727d59dd8c1c88feac9fc7fe0
Dmitry Dyakov <akk.dyakova@mail.ru>
2022-08-08 01:09:44 +0300

    решил первую задачу

--- dz_sem_01/zadacha01/Program.cs
+++ dz_sem_01/zadacha01/Program.cs
@@ -1,15 +1,15 @@
-﻿int a = 5, b = 8;
-Console.WriteLine("Введите число a: ");
-Console.ReadLine();
+﻿Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число b: ");
-Console.ReadLine();
+int b = Convert.ToInt32(Console.ReadLine());
 
 
 if (a > b)
-{
-    Console.WriteLine($" max = {a} , min = {b}");
-}
-else 
-{
-    Console.WriteLine($" max = {b} ,min = {a}");
-}
+    {
+    Console.WriteLine($" max = {a} , min = {b} ");
+    }
+    else 
+    {
+    Console.WriteLine($" max = {b} ,min = {a} ");
+    }
+  
