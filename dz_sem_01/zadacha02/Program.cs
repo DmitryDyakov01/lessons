019d666361bf917aa0d5270c91a609007d22b2d8
Dmitry Dyakov <akk.dyakova@mail.ru>
2022-08-08 04:11:44 +0300

    Написал код ко второй задаче

--- dz_sem_01/zadacha02/Program.cs
+++ dz_sem_01/zadacha02/Program.cs
@@ -1,3 +1,14 @@
-﻿Console.WriteLine("Привет, !");
-Console.ReadLine();
-Console.WriteLine("ReadLine()");
+﻿Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число с: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+
+Console.WriteLine(max);
