3fdec0260069c2e02e83763355e379016c7eb44b
Dmitry Dyakov <akk.dyakova@mail.ru>
2022-08-08 06:24:03 +0300

    Задание 4

---
+++ dz_sem_01/zadacha04/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Введите число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+if (count == 1)
+{
+    count +=1;
+}
+
+
+while (count <= value)
+{
+    Console.Write($"{count} "+"");
+    count+=2;
+}
