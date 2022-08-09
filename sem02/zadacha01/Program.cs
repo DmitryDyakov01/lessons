150298fb5f689d00b8dea23c5c2d42fdaab8b0ae
Dmitry Dyakov <akk.dyakova@mail.ru>
2022-08-09 19:46:21 +0300

    Изменил

---
+++ sem02/zadacha01/Program.cs
@@ -0,0 +1,7 @@
+﻿
+Console.WriteLine("Введите трехзначное число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int n1 = (n/10)%10;
+
+Console.WriteLine(n1);
---
+++ sem02/zadacha02/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введи число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string ntxt  = Convert.ToString(n);
+if (ntxt.Length > 2)
+{
+  Console.WriteLine(ntxt[2]);
+}
+else
+{
+  Console.WriteLine(" Третьей цифры нет");
+}
+
+
