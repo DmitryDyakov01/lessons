f1c1d73ba53921671c0a418ec6649bf8df196fb2
Dmitry Dyakov <akk.dyakova@mail.ru>
2022-08-29 17:23:29 +0300

    Семинар четвертый

---
+++ sem04/zadacha03/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] array = new int[8];
+Random RandomNumber = new Random();
+Console.WriteLine();
+for (int i = 0; i < array.Length; i++)
+{
+array[i] = RandomNumber.Next(1, 100);
+Console.Write("{0} ", array[i]);
+}
