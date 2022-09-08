adfc598b30edb2ba228209a812e7855eb1e2c8aa
Dmitry Dyakov <akk.dyakova@mail.ru>
2022-09-08 12:22:03 +0300

    Семинар девятый

---
+++ sem09/zadacha01/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (m>n)
+for (int i = n; i <= m; i++)
+    Console.Write($" {i}");
+else
+    for (int i = m; i <= n; i++)
+        Console.Write($" {i}");
